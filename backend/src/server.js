ade77dcdafb36010bac2db693d205e885698ed65
manan sanchapara <mananheer8@gmail.com>
2026-04-30 18:11:04 +0530

    render deployment path fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,12 @@ import path from "path";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const frontendPath = path.resolve(__dirname, "../../frontend/dist");
 const app = express();
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.get("/healthz",(req, res) =>{
     res.status(200).json({msg: "sucess a"});
@@ -15,14 +19,20 @@ app.get("/b",(req, res) =>{
     res.status(200).json({msg: "sucess b"});
 });
 
-if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+// if(ENV.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-    app.get("*",(req, res) => {
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+//     app.get("*",(req, res) => {
+//         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+//     });
+// }
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(frontendPath));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(frontendPath, "index.html"));
     });
 }
-
 
 const startServer = async() => {
     try {
