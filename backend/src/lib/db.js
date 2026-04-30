a6cb65e889c2c4bac73b962f39040bdd3c549626
manan sanchapara <mananheer8@gmail.com>
2026-04-30 17:04:30 +0530

    validation check added for DB_URL

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,11 +4,14 @@ import { ENV } from "./env.js";
 
 export const connectDB = async() => {
     try {
+        if (!ENV.DB_URL) {
+            throw new Error("Missing required env var: MONGO_URL");
+        }
         const conn = await mongoose.connect(ENV.DB_URL);
         console.log("connected to MongoDB ", conn.connection.host);
     } catch (error) {
-        console.log("error connecting to MongoDB ", error);
-        process.exit(1);
+        console.error("error connecting to MongoDB", error instanceof Error ? error.message : error);
+        throw error;
 
     }
 }
