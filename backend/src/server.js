7fe7c39b876244bb7e844baa1e95d7065a5191a9
manan sanchapara <mananheer8@gmail.com>
2026-05-04 16:08:46 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,23 @@
 import express from "express"
 import path from "path";
+import cors from "cors"
+import { serve } from "inngest/express"
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-
+import { inngest } from "./lib/inngest.js";
 import { fileURLToPath } from 'url';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const frontendPath = path.resolve(__dirname, "../../frontend/dist");
 const app = express();
 // const __dirname = path.resolve();
+
+// Middleware
+app.use(express.json())
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true}))
+app.use("api/inngest", serve({ client: inngest, functions}));
 
 app.get("/healthz",(req, res) =>{
     res.status(200).json({msg: "sucess a"});
@@ -19,13 +27,7 @@ app.get("/b",(req, res) =>{
     res.status(200).json({msg: "sucess b"});
 });
 
-// if(ENV.NODE_ENV === "production"){
-//     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-//     app.get("*",(req, res) => {
-//         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-//     });
-// }
 if (ENV.NODE_ENV === "production") {
     app.use(express.static(frontendPath));
 
