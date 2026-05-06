47dedd02781ed6f05a49f89bc07143d21fb6746e
manan sanchapara <mananheer8@gmail.com>
2026-05-06 12:36:43 +0530

    implemented server side authentication

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,13 @@ import express from "express"
 import path from "path";
 import cors from "cors"
 import { serve } from "inngest/express"
+import { clerkMiddleware } from "@clerk/express" 
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
 import { fileURLToPath } from 'url';
+import chatRoutes  from "./routes/chatRoutes.js"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,15 +19,18 @@ const app = express();
 // Middleware
 app.use(express.json())
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true}))
+app.use(clerkMiddleware()); // this adds the auth field to requests
+
 app.use("/api/inngest", serve({ client: inngest, functions}));
+app.use("/api/chat", chatRoutes)
 
 app.get("/healthz",(req, res) =>{
-    res.status(200).json({msg: "sucess a"});
+    res.status(200).json({ msg: "api is up and running" });
 });
 
-app.get("/b",(req, res) =>{
-    res.status(200).json({msg: "sucess b"});
-});
+
+
+
 
 
 if (ENV.NODE_ENV === "production") {
