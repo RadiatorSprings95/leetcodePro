96447567add94055167fd685df6789b941a9bab7
altSprings <mananheer8@gmail.com>
2026-05-19 18:47:33 +0530

    shift to pc

--- frontend/src/pages/ProblemsPage.jsx
+++ frontend/src/pages/ProblemsPage.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { Link } from "react-router"
+import Navbar from '../components/Navbar'
+
+import { PROBLEMS } from "../data/problems";
+import { Code2Icon } from "lucide-react";
 
 function ProblemsPage() {
+const problems = Object.values(PROBLEMS);
+
   return (
-    <div>
-      problemssmsd
+    <div className='min-h-screen bg-base-200'>
+      <Navbar/>
+
+      <div className='max-w-6xl mx-auto px-4 py-12'>
+        {/* Header */}
+        <div className='mb-8'>
+          <h1 className='text-4xl font-bold  mb-2'>Practice Problems</h1>
+          <p className='text-base-content/70'>
+            Engage in some Mind Woggling Sets of DSA Question 
+          </p>
+        </div> 
+
+        {/* Problem List*/}
+        <div className='space-y-4'>
+          {problems.map(problem => (
+            <Link
+              key={problem.id}
+              to={`/problem/${problem.id}`}
+              className="card bg-base-100 hover:scale-[1.01] transition-transform"
+            >
+              <div className="card-body">
+                <div className="flex items-center justify-between gap-4">
+                  {/* Left side*/}
+                  <div className="flex-1">
+                    <div className="flex items-center gap-3 mb-2">
+                      <div className="size-12 rounded-lg bg-primary/10 flex items-center justify-between">
+                        <Code2Icon className="size-6 text-primary"/>
+                      </div>
+                      <div className="flex-1">
+                        <div className="flex items-center gap-2 mb-1">
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+
+
+      </div>
     </div>
   )
 }
